b4cca2ead15cac9d693a4dae85612047c06db2a7
darrylbilongo <darrylbilongo@gmail.com>
2019-10-06 23:15:37 +0200

    struture de base + bootstrap

---
+++ Web/easygame/src/components/Menus.js
@@ -0,0 +1,20 @@
+import React from 'react'; 
+
+class Menus extends React.Component {
+  
+  // data needed
+  
+  state = {
+
+  };
+
+  render(){
+    return (
+    <div>
+        <h1>Hello World</h1>
+    </div>
+    );
+  }
+}
+
+export default Menus;
