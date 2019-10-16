f45e4c0600cddbfd62f33647b02ce9e30bccdcf2
darrylbilongo <darrylbilongo@gmail.com>
2019-10-16 21:51:01 +0200

    update calendar

---
+++ Web/easygame/src/components/NavigationBar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Nav, Navbar } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+  .navbar {
+    background-color: #555;
+  }
+  a, .navbar-brand, .navbar-nav .nav-link {
+    color: #aaa;
+    &:hover {
+      color: white;
+    }
+  }
+`;
+
+export const NavigationBar = () => (
+  <Styles>
+    <Navbar expand="lg">
+      <Navbar.Brand href="/">Easy Game</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/">Home</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/admin">Administration</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/about">About</Link>
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </Styles >
+)
