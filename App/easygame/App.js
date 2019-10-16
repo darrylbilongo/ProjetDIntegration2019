77cc015d38164530237e7f3db1e7dacd406d8daa
Magnes Simon <55548308+MagnesSimon@users.noreply.github.com>
2019-10-16 18:36:48 +0200

    Drawer Navigator
    ajout du components SettingsScreen,
    ajout du drawer navigator dans app.js mais erreur que je n'arrive pas à corriger

--- App/easygame/App.js
+++ App/easygame/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { createDrawerNavigator, createAppContainer } from 'react-navigation-stack';
+import DrawerNavigator from 'react-navigation'
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView
+} from 'react-native'
+
 import NavigationRoot from './src/components/NavigationRoot';
 import NavigationService from './src/components/NavigationService';
+import HomeScreen from './src/components/Homescreen'
+import SettingsScreen from './src/components/SettingsScreen'
 
 export default class App extends React.Component {
   render() {
-    return <NavigationRoot 
+    return <NavigationRoot
             ref={navigatorRef => {
               NavigationService.setTopLevelNavigator(navigatorRef);
             }}
-          />;
-  }
-}
+        />;
+    }
+ }
+// ERREUR INCOMPREHENSIBLE
+// DrawerNavigator.create is not a function, is unedefined
+// const MyApp = DrawerNavigator({
+//   Home: {
+//     screen: HomeScreen,
+//   },
+//  Settings:{
+//     screen: SettingsScreen,
+//   }
+// })
