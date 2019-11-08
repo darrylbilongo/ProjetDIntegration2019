1a8e133e87e143b41c3d26fd98f0bc60959e0159
Manste <manuelle.ndamtang@yahoo.com>
2019-11-08 16:12:52 -0500

    correction pour l'api

---
+++ BackendApi/models/users.model.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    nomUtilisateur: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength:5
+    },
+    nom: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    prenom: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    motDePasse: {
+        type: String,
+        required: true,
+    },
+    dateNaissance: {
+        type: Date,
+        required: true,
+    },
+    estSupprime: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    totem:{
+        type: String,
+        required: true,
+    },
+    fonction:{
+        type: String,
+        default: 'scout'
+    }
+
+},{
+    timestamps: true   
+});
+
+const users = mongoose.model('users', userSchema);
+
+module.exports = users;
