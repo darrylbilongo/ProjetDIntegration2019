fffc11ad8ef9dc21c59ab45a1e6c4044fa8c1222
Manste <manuelle.ndamtang@yahoo.com>
2019-11-08 17:19:20 -0500

    mise a jour de l'api

--- BackendApi/server.js
+++ BackendApi/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 5000;
 
 const uri = process.env.SRV_URI;
 mongoose.connect(uri, {useNewUrlParser: true,
