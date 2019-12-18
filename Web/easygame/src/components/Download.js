4d11856bf3cabf55a841257f9d1cfe01a3c74e9a
darrylbilongo <darrylbilongo@gmail.com>
2019-12-18 01:42:24 +0100

    bouton download

--- Web/easygame/src/components/Download.js
+++ Web/easygame/src/components/Download.js
@@ -4,8 +4,8 @@ export class Download extends Component {
     render() {
         return (
             <div className="container">
-                <h1>Téléchargez l'application</h1>
-                <button className="btn btn-danger">
+                <h1>Téléchargez l'application Easygame</h1>
+                <button className="btn btn-secondary">
                     <a href='/files/easygameApp.apk' download>Click to download</a>
                 </button>
             </div>
